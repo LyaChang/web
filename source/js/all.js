7bc708a1a9cd637fb5badeb31bbf9a28bc98b91c
lya <lyachang006@gmail.com>
2019-01-23 21:22:05 +0800

    “分類動態”

--- source/js/all.js
+++ source/js/all.js
@@ -16,5 +16,42 @@ $(document).ready(function(){
     $('.kind_lists li a').on('click',(function(){
         var name = $(this).data("value")
         console.log(name)
+
+        if(name == "web"){
+            $(".web").css("display","block");
+            $(".banner,.app").css("display","none");
+            TweenMax.to(".web",1,{opacity:"1",ease: Power2.easeOut});
+            TweenMax.to(".app",0.6,{opacity:"0",ease: Power2.easeOut});
+            TweenMax.to(".banner",0.6,{opacity:"0",ease: Power2.easeOut});
+            $(".kind_lists a").removeClass("active");
+            $(".segment-1 a").addClass("active");
+        }
+        else if(name == "app"){
+            $(".web,.banner").css("display","none");
+            $(".app").css("display","block");
+            TweenMax.to(".app",0.6,{opacity:"1",ease: Power2.easeOut});
+            TweenMax.to(".web",0.6,{opacity:"0",ease: Power2.easeOut});
+            TweenMax.to(".banner",0.6,{opacity:"0",ease: Power2.easeOut});
+            $(".kind_lists a").removeClass("active");
+            $(".segment-2 a").addClass("active");
+        }
+        else if(name == "banner"){
+            $(".web,.app").css("display","none");
+            $(".banner").css("display","block");
+            TweenMax.to(".app",0.6,{opacity:"0",ease: Power2.easeOut});
+            TweenMax.to(".web",0.6,{opacity:"0",ease: Power2.easeOut});
+            TweenMax.to(".banner",0.6,{opacity:"1",ease: Power2.easeOut});
+            
+            $(".kind_lists a").removeClass("active");
+            $(".segment-3 a").addClass("active");
+        }
+        else{
+            $(".banner,.app,.web").css("display","block");
+            TweenMax.to(".web,.app,.banner",0.6,{opacity:"1",ease: Power2.easeOut});
+            
+            $(".kind_lists a").removeClass("active");
+            $(".segment-0 a").addClass("active");
+        }
+        
     }))
 })
