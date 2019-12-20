6c9de0c03f75e07c4dd390ae18e842c160d24690
lya <lyachang006@gmail.com>
2019-12-20 17:00:06 +0800

    再版

--- source/js/all.js
+++ source/js/all.js
@@ -30,14 +30,14 @@ function masonry_fn(){
 };
 
 function errorOne() {
-    console.log('first image load error!');
+    // console.log('first image load error!');
   }
   
 
 $(document).ready(function(){
     $('.kind_lists_work li a').on('click',(function(){
         var name = $(this).data("value")
-        console.log(name)
+        // console.log(name)
 
         if(name == "web"){
             $(".web").css("display","block");
@@ -81,12 +81,12 @@ $(document).ready(function(){
 
         $('.kind_lists_banner li a').on('click',(function(){
             var name = $(this).data("value")
-            console.log(name)
+            // console.log(name)
     
             if(name == "work"){
                 $(".work").css("display","block");
-                $(".act,.test,.travel").css("display","none");
-                $(".act,.test,.travel").hide();
+                $(".act,.test,.travel,.pd").css("display","none");
+                $(".act,.test,.travel,.pd").hide();
                 $(".work").show();
                
                 masonry_fn();
@@ -94,13 +94,14 @@ $(document).ready(function(){
                 TweenMax.to(".test",1,{opacity:"0",ease: Power2.easeOut});
                 TweenMax.to(".act",1,{opacity:"0",ease: Power2.easeOut});
                 TweenMax.to(".travel",1,{opacity:"0",ease: Power2.easeOut});
+                TweenMax.to(".pd",1,{opacity:"0",ease: Power2.easeOut});
                 $(".kind_lists_banner a").removeClass("active");
                 $(".segment-1 a").addClass("active");
             }
             else if(name == "test"){
-                $(".work,.act,.travel").css("display","none");
+                $(".work,.act,.travel,.pd").css("display","none");
                 $(".test").css("display","block");
-                $(".work,.act,.travel").hide();
+                $(".work,.act,.travel,.pd").hide();
                 $(".test").show();
               
                 masonry_fn();
@@ -108,13 +109,14 @@ $(document).ready(function(){
                 TweenMax.to(".work",1,{opacity:"0",ease: Power2.easeOut});
                 TweenMax.to(".act",1,{opacity:"0",ease: Power2.easeOut});
                 TweenMax.to(".travel",1,{opacity:"0",ease: Power2.easeOut});
+                TweenMax.to(".pd",1,{opacity:"0",ease: Power2.easeOut});
                 $(".kind_lists_banner a").removeClass("active");
                 $(".segment-2 a").addClass("active");
             }
             else if(name == "act"){
-                $(".work,.test,.travel").css("display","none");
+                $(".work,.test,.travel,.pd").css("display","none");
                 $(".act").css("display","block");
-                $(".work,.test,.travel").hide();
+                $(".work,.test,.travel,.pd").hide();
                 $(".act").show();
                
                 masonry_fn();
@@ -122,30 +124,48 @@ $(document).ready(function(){
                 TweenMax.to(".work",1,{opacity:"0",ease: Power2.easeOut});
                 TweenMax.to(".act",1,{opacity:"1",ease: Power2.easeOut});
                 TweenMax.to(".travel",1,{opacity:"0",ease: Power2.easeOut});
+                TweenMax.to(".pd",1,{opacity:"0",ease: Power2.easeOut});
                 
                 $(".kind_lists_banner a").removeClass("active");
                 $(".segment-3 a").addClass("active");
             }
             else if(name == "travel"){
-                $(".work,.test,.act").css("display","none");
+                $(".work,.test,.act,.pd").css("display","none");
                 $(".travel").css("display","block");
-                $(".work,.test,.act").hide();
+                $(".work,.test,.act,.pd").hide();
                 $(".travel").show;
                
                 masonry_fn();
                 TweenMax.to(".test",1,{opacity:"0",ease: Power2.easeOut});
                 TweenMax.to(".work",1,{opacity:"0",ease: Power2.easeOut});
                 TweenMax.to(".act",1,{opacity:"0",ease: Power2.easeOut});
+                TweenMax.to(".pd",1,{opacity:"0",ease: Power2.easeOut});
                 TweenMax.to(".travel",1,{opacity:"1",ease: Power2.easeOut});
                 
                 $(".kind_lists_banner a").removeClass("active");
                 $(".segment-4 a").addClass("active");
             }
-            else{
-                $(".work,.test,.act,.travel").css("display","block");
-                $(".work,.test,.act,.travel").show();
+            else if(name == "pd"){
+                $(".work,.test,.act,.travel").css("display","none");
+                $(".pd").css("display","block");
+                $(".work,.test,.act,.travel").hide();
+                $(".pd").show;
+               
                 masonry_fn();
-                TweenMax.to(".work,.test,.act,.travel",1,{opacity:"1",ease: Power2.easeOut});
+                TweenMax.to(".test",1,{opacity:"0",ease: Power2.easeOut});
+                TweenMax.to(".work",1,{opacity:"0",ease: Power2.easeOut});
+                TweenMax.to(".act",1,{opacity:"0",ease: Power2.easeOut});
+                TweenMax.to(".travel",1,{opacity:"0",ease: Power2.easeOut});
+                TweenMax.to(".pd",1,{opacity:"1",ease: Power2.easeOut});
+                
+                $(".kind_lists_banner a").removeClass("active");
+                $(".segment-5 a").addClass("active");
+            }
+            else{
+                $(".work,.test,.act,.travel,.pd").css("display","block");
+                $(".work,.test,.act,.travel,.pd").show();
+                masonry_fn();
+                TweenMax.to(".work,.test,.act,.travel,.pd",1,{opacity:"1",ease: Power2.easeOut});
                 
                 $(".kind_lists_banner a").removeClass("active");
                 $(".segment-0 a").addClass("active");
@@ -180,11 +200,11 @@ $(document).ready(function(){
         var name = $(this).data("value")
         if(name == "work"){
             $(".dropmenu .words").html("工作轉職");
-            $(".dropmenu span").html("7");
+            $(".dropmenu span").html("6");
         }
         else if(name == "all"){
             $(".dropmenu .words").html("ALL");
-            $(".dropmenu span").html("22");
+            $(".dropmenu span").html("31");
         }
         else if(name == "test"){
             $(".dropmenu .words").html("測驗分析");
@@ -192,11 +212,15 @@ $(document).ready(function(){
         }
         else if(name == "act"){
             $(".dropmenu .words").html("活動");
-            $(".dropmenu span").html("8");
+            $(".dropmenu span").html("5");
         }
         else if(name == "travel"){
             $(".dropmenu .words").html("旅遊");
             $(".dropmenu span").html("1");
+        }
+        else if(name == "pd"){
+            $(".dropmenu .words").html("商品");
+            $(".dropmenu span").html("13");
         }
     }));
     $('.dropmenu_space .cancel_btn').on('click',(function(){
@@ -206,19 +230,30 @@ $(document).ready(function(){
     //nav
 
     $('#nav').on('click',(function(){
-        console.log("nav");
+        // console.log("nav");
+        TweenMax.to(".nav_mobile",.6,{opacity:"0.94",ease: Power2.easeOut,"display":"block"});        
+    }));
+    $('.nav_mobile .cancel_btn').on('click',(function(){
+        // console.log("nav");
+        TweenMax.to(".nav_mobile",.6,{opacity:"0",ease: Power2.easeOut,"display":"none"});        
     }));
 
     masonry_fn();
-    $('.gif').gifplayer({label:""});
+    //$('.gif').gifplayer({label:""});
 
-    $(window).resize(function(){
+    if ( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+
+    }
+    else {
+        $(window).resize(function(){
         
-        //改變視窗時重新整理一次
-        this.location.reload();
-        masonry_fn();
-    });
-    
+            //改變視窗時重新整理一次
+            this.location.reload();
+            masonry_fn();
+        });
+    }
+    lazyload();
+
 
 })
 
